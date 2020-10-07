a4879a0ee9c9b57b9703bffe9352aa3507cf151c
Amirreza <davari.am@gmail.com>
2020-10-07 21:42:21 +0330

    v1

---
+++ v1.py
@@ -0,0 +1,11 @@
+tehran_cbp = ['cbp1','cbp2','cbp3','cbp4','cbp5','cbp6','cbp7','cbp8','cbp9','cbp10','cbp11','cbp12']
+str = "cm_acct_balance_cbp10_2020.csv.gz"
+z=0
+for i in str:
+    print(i.isdigit(),z)
+    z = z+1
+
+
+#str.index ('cbp1')
+#print(str.find('cbp1'))
+#print(str.index ('cbp1_'))
