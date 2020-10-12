dde8a4f8c50a881f2141547c7089c7de556d39b9
Amirreza <davari.am@gmail.com>
2020-10-12 19:33:20 +0330

    v1

--- v2.py
+++ v2.py
@@ -1,21 +1,21 @@
 import fnmatch
 import os
+
 ep_file_list = []
 ep_list = ['ep1f','ep2f','ep3f','ep4f']
 
 for file in os.listdir('.'):
-    if fnmatch.fnmatch(file, 'ep*.txt'):
+    if fnmatch.fnmatch(file,'ep*.txt'):
         ep_file_list.append(file)
 
 cbp = list(range(1,13,1))
 z = 0
 for item in cbp:
     cbp[z] = str(item)
-    z = z+1
-    
-    
+    z = z + 1
+
 for item_name in ep_file_list:
-    file_list = open(item_name, "r")
+    file_list = open(item_name,"r")
     cbp_list = []
 
     for item in file_list:
@@ -31,16 +31,14 @@ for item_name in ep_file_list:
                     cbp_number = item[z]
                     cbp_list.append(cbp_number)
                     break
-            z = z+1
+            z = z + 1
     file_list.close()
-
-
 
     with open(item_name) as f:
         first_line = f.readline()
     f.close()
 
-    for index, item in enumerate(first_line):
+    for index,item in enumerate(first_line):
 
         if item.isdigit():
             Index_list = index
@@ -49,13 +47,13 @@ for item_name in ep_file_list:
     not_existed = list(set(cbp) - set(cbp_list))
     if ep_list[0] == item_name[0:4]:
         for item in not_existed:
-            print(first_line[0:Index_list-3]+" file is missing in: cbp" + item )
+            print(first_line[0:Index_list - 3] + " file is missing in: cbp" + item)
     elif ep_list[1] == item_name[0:4]:
         for item in not_existed:
-            print(first_line[0:Index_list-4]+" file is missing in: mcbp" + item )
+            print(first_line[0:Index_list - 4] + " file is missing in: mcbp" + item)
     elif ep_list[2] == item_name[0:4]:
         for item in not_existed:
-            print(first_line[0:Index_list-4]+" file is missing in: scbp" + item )
+            print(first_line[0:Index_list - 4] + " file is missing in: scbp" + item)
     elif ep_list[3] == item_name[0:4]:
         for item in not_existed:
-            print(first_line[0:Index_list-4]+" file is missing in: tcbp" + item )
+            print(first_line[0:Index_list - 4] + " file is missing in: tcbp" + item)
