bc7092dede6e8bc53c8f74b43867eddc03ad18f8
Amirreza <davari.am@gmail.com>
2020-10-13 20:20:46 +0330

    v1

--- v1.py
+++ v1.py
@@ -1,4 +1,5 @@
 import fnmatch
+import re
 import os
 ep_file_list = []
 ep_list = ['ep1f','ep2f','ep3f','ep4f']
@@ -9,13 +10,37 @@ for file in os.listdir('.'):
 
 cbp = list(range(1,13,1))
 z = 0
+
 for item in cbp:
     cbp[z] = str(item)
     z = z+1
-    
-    
+
 for item_name in ep_file_list:
     file_list = open(item_name, "r")
+
+    file_result= re.search(r"^.*cm_credit_limit_in.txt$",item_name)
+    if file_result:
+        list_final_file = []
+
+        f = open(item_name,"r")
+        text = f.readlines()
+        f.close
+        for index,item in enumerate(text):
+            result = re.search(r"cm_credit_limit_accm.*",item)
+            result2 = re.search(r"cm_credit_limit_value.*",item)
+
+            if result or result2:
+                list_final_file.append(index)
+
+        for list_item in reversed(list_final_file):
+           del text[int(list_item)]
+        new_file = open(item_name,"w+")
+        for line in text:
+            new_file.write(line)
+        new_file.close()
+
+
+
     cbp_list = []
 
     for item in file_list:
@@ -33,8 +58,6 @@ for item_name in ep_file_list:
                     break
             z = z+1
     file_list.close()
-
-
 
     with open(item_name) as f:
         first_line = f.readline()
